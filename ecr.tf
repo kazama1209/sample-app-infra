f45a1393a632e4c1e6edbd59f14d3f645321b40d
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2020-09-19 22:28:41 +0900

    update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -10,6 +10,28 @@ resource "aws_ecr_repository" "sample-app-rails" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "sample-app-rails" {
+  repository = "${aws_ecr_repository.sample-app-rails.name}"
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 images",
+        "selection": {
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
+
 # Nginx
 resource "aws_ecr_repository" "sample-app-nginx" {
   name                 = "${var.prefix}-nginx"
