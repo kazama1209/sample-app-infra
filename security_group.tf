3cdc4cce39ac74e679ec2b9da83483e0151ca798
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2020-09-19 22:17:31 +0900

    update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -6,31 +6,23 @@ resource "aws_security_group" "sample-app-sg" {
   description = "${var.prefix}-sg"
   vpc_id      = "${aws_vpc.sample-app-vpc.id}"
   
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.prefix}-sg"
   }
-}
-
-resource "aws_security_group_rule" "sample-app-sg-ingress" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
-
-  security_group_id = "${aws_security_group.sample-app-sg.id}"
-}
-
-resource "aws_security_group_rule" "sample-app-sg-egress" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = "${aws_security_group.sample-app-sg.id}"
 }
 
 # ロードバランサー
@@ -39,31 +31,23 @@ resource "aws_security_group" "sample-app-alb-sg" {
   description = "${var.prefix}-alb-sg"
   vpc_id      = "${aws_vpc.sample-app-vpc.id}"
   
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.prefix}-alb-sg"
   }
-}
-
-resource "aws_security_group_rule" "sample-app-alb-sg-ingress" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
-
-  security_group_id = "${aws_security_group.sample-app-alb-sg.id}"
-}
-
-resource "aws_security_group_rule" "sample-app-alb-sg-egress" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = "${aws_security_group.sample-app-alb-sg.id}"
 }
 
 # データベース
@@ -72,29 +56,21 @@ resource "aws_security_group" "sample-app-db-sg" {
   description = "${var.prefix}-db-sg"
   vpc_id      = "${aws_vpc.sample-app-vpc.id}"
   
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.prefix}-db-sg"
   }
-}
-
-resource "aws_security_group_rule" "sample-app-db-sg-ingress" {
-  type        = "ingress"
-  from_port   = 3306
-  to_port     = 3306
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
-
-  security_group_id = "${aws_security_group.sample-app-db-sg.id}"
-}
-
-resource "aws_security_group_rule" "sample-app-db-sg-egress" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = "${aws_security_group.sample-app-db-sg.id}"
 }
